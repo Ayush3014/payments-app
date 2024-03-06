fbd907ee079e976974a9fe5562166e65978a60fd
Ayush <ayushpanchal005@gmail.com>
2024-03-06 16:04:00 +0530

    Added middleware

---
+++ backend/middleware.js
@@ -0,0 +1,28 @@
+import { JWT_SECRET } from './config';
+const jwt = require('jsonwebtoken');
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    return res.status(403).json({});
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if (decoded) {
+      req.userId = decoded.userId;
+      next();
+    } else {
+      res.status(403).json({});
+      return;
+    }
+  } catch (error) {
+    res.status(403).json({});
+  }
+}
+
+module.exports = {
+  authMiddleware,
+};
