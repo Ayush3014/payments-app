99ef05c8dc70b3bb5c016f05a632a085b2f7071a
Ayush <ayushpanchal005@gmail.com>
2024-03-06 23:26:07 +0530

    Added the signup page and its components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+// import Dashboard from './pages/Dashboard';
+// import SendMoney from './pages/SendMoney';
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="signup" element={<SignUp />} />
+          {/* <Route path="signin" element={<SignIn />} /> */}
+          {/* <Route path="dashboard" element={<Dashboard />} /> */}
+          {/* <Route path="send" element={<SendMoney />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
